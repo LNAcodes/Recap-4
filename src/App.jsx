eec81850c5f3c35478cdfc2df9fddc323e8523ac
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-01-27 11:51:31 +0100

    Add form to create new colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+import { uid } from "uid";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  function handleAddColor(newColorData) {
+    const newColor = {
+      id: uid(),
+      role: newColorData.role,
+      hex: newColorData.hex,
+      contrastText: newColorData.contrastText,
+    };
+    setColors([newColor, ...colors]);
+  }
+
   return (
     <>
       <h1>Theme Creator</h1>
-      {initialColors.map((color) => (
+      <ColorForm onSubmitColor={handleAddColor} />
+      {colors.map((color) => (
         <Color key={color.id} color={color} />
       ))}
     </>
